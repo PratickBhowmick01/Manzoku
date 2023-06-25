edc4882d195ce20154ce8e888072099080a0c6a4
mehnazara <mehnazfazal952@gmail.com>
2023-06-25 20:12:00 +0600

    'Product API'

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,20 @@
+const Product = require("../models/productModel");
+
+
+
+//Create Product
+exports.createProduct = async (req,res,next)=>{
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product 
+    })
+
+}
+
 exports.getAllProducts =(req,res) =>{
 
     res.status(200).jason({message:"Route is working fine"});
-} 
+}
