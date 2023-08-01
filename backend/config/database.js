f42a58ebc50400281418c90c872ef9d6b75decb1
mehnazara <mehnazfazal952@gmail.com>
2023-08-02 00:28:49 +0600

    Order API (With Admin Controls)

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose =require("mongoose");
 
-const database =module.exports =() =>{
+const database = module.exports =() =>{
     const connectonParams ={
         useNewUrlParser:true,
         useUnifiedTopology:true
