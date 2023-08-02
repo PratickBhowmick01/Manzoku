778f616c5d09a68331d9828409f46010efdcad91
mehnazara <mehnazfazal952@gmail.com>
2023-08-02 23:29:32 +0600

    Actual Frontend Landing Page

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Header from "./component/layout/Header/Header.js";
+import {BrowserRouter as Router} from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+import Footer from "./component/layout/Footer/Footer.js"
+
+
+
+function App() {
+  
+  
+  React.useEffect(() => {
+    WebFont.load({
+      google:{
+        families:["Book Antiqua","Droid Sans"]
+      }
+    });
+  }, []);
+
+
+  return (
+    <Router>
+      <Header />
+
+      <Footer />
+    </Router>
+   
+  );
+}
+
+export default App;
